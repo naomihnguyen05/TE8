c814324a14f9ead2e5140759551378f03ca6a0d1
naomihnguyen05 <100325630+naomihnguyen05@users.noreply.github.com>
2022-11-06 12:49:15 -0800

    Copy Commit

---
+++ scripts/client.js
@@ -0,0 +1,61 @@
+// Art 109 Three.js Demo Site
+// client.js
+// A basic three.js scene which displays and rotates a polygon with a wireframe
+
+// Extract globals from external script
+const { THREE } = window;
+
+// Create a scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  50,
+  500 / 500,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xDED5CA);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(500, 500);
+document.body.appendChild(renderer.domElement);
+
+// Add a polygon to the scene
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+const material = new THREE.MeshBasicMaterial( { color: 0xDAF7A6 } );
+const torusKnot = new THREE.Mesh( geometry, material );
+scene.add( torusKnot );
+
+// add wireframe to shape
+const matLineBasic = new THREE.LineBasicMaterial({ color: 0xAEC685 });
+const wireframe = new THREE.WireframeGeometry(geometry);
+const line = new THREE.LineSegments(wireframe, matLineBasic);
+line.material.depthTest = false;
+line.material.opacity = 0.9;
+line.material.transparent = false;
+scene.add(line);
+
+// Position our camera so we can see the shape
+camera.position.z = 5;
+
+// Add a directional light to the scene
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+scene.add(directionalLight);
+
+// Add an ambient light to the scene
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.95);
+scene.add(ambientLight);
+
+// Define and then call the render loop
+// Define
+function render() {
+  requestAnimationFrame(render);
+
+  // Rotate our shape
+  torusKnot.rotation.x += 0.005;
+  torusKnot.rotation.y += 0.005;
+  line.rotation.x += 0.005;
+  line.rotation.y += 0.005;
+  renderer.render(scene, camera);
+}
+// Call
+render();
---
+++ scripts/client2.js
@@ -0,0 +1,121 @@
+// Art 109 Three.js Demo Site
+// client2.js
+// A three.js scene which loads a custom GLTF model and implements Orbit controls
+
+// Import required source code
+// Import three.js core
+import * as THREE from "../build/three.module.js";
+
+// Import add-ons for GLTF models and orbit controls
+import { OrbitControls } from "../src/OrbitControls.js";
+import { GLTFLoader } from "../src/GLTFLoader.js";
+
+//Identify div in HTML to place scene
+var container = document.getElementById("space");
+
+//Create scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xdfdfdf);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+//renderer.setSize(400, 800);
+// Add scene to gltf.html
+container.appendChild(renderer.domElement);
+
+// Material to be added to model
+var newMaterial = new THREE.MeshStandardMaterial({ color: 0x2E5939 });
+
+// Variable for GLTF data
+var mesh;
+var mesh2;
+
+// Load GLTF model, add material, and add it to the scene
+const loader = new GLTFLoader().load(
+  "../../assets/BLOB.glb",// comment this line out and un comment the line below to swithc models
+  //"./assets/gourd_web.glb", //<-- photogrammetery model
+  function(gltf) {
+    // Scan loaded model for mesh and apply defined material if mesh is present
+    gltf.scene.traverse(function(child) {
+      if (child.isMesh) {
+        //child.material = newMaterial;
+      }
+    });
+    // set position and scale
+    mesh = gltf.scene;
+    mesh.position.set(0, -0.1, 0);
+    mesh.rotation.set(180, 0, 0);
+    mesh.scale.set(.5, .5, .5); // <-- change this to (1, 1, 1) for photogrammetery model
+    // Add model to scene
+    scene.add(mesh);
+  },
+  undefined,
+  function(error) {
+    console.error(error);
+  }
+);
+
+// Load GLTF model, add material, and add it to the scene
+const loader2 = new GLTFLoader().load(
+  "../../assets/BLOB.glb",// comment this line out and un comment the line below to swithc models
+  //"./assets/gourd_web.glb", //<-- photogrammetery model
+  function(gltf2) {
+    // Scan loaded model for mesh and apply defined material if mesh is present
+    gltf2.scene.traverse(function(child) {
+      if (child.isMesh) {
+        //child.material = newMaterial;
+      }
+    });
+    // set position and scale
+    mesh2 = gltf2.scene;
+    mesh2.position.set(2, 2, 2);
+    mesh2.rotation.set(0, 0, 0);
+    mesh2.scale.set(.2, .2, .2); // <-- change this to (1, 1, 1) for photogrammetery model
+    // Add model to scene
+    scene.add(mesh2);
+  }
+);
+
+// Add Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 3;
+controls.maxDistance = 6;
+controls.target.set(0, 0, -0.2);
+controls.update();
+
+// Position our camera so we can see the shape
+camera.position.z = 4.5;
+
+// Add a directional light to the scene
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+scene.add(directionalLight);
+
+// Add an ambient light to the scene
+const ambientLight = new THREE.AmbientLight(0x1A00FF, 0.99);
+scene.add(ambientLight);
+
+// Define and call the render loop
+// Define
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+// Call
+render();
+
+// Respond to Window Resizing
+window.addEventListener("resize", onWindowResize);
+
+// Window resizing function
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  render();
+}
